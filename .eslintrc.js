480529edf5f8603c488b3ae4371a471c38e4785f
wzg15801407309 <1051084301@qq.com>
2020-04-21 15:59:53 +0800

    删除u不要的东西

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,21 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
+  'extends': [
     'plugin:vue/essential',
-    '@vue/standard'
+    'eslint:recommended'
   ],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // //强制使用单引号
+    // quotes: ['error', 'single'],
+    //在结尾处，强制不使用分号结尾
+    // semi: ['error', 'never']
+    //使用分号
+    semi: ['error', 'always']
   }
 }
